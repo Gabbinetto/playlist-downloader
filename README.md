c8a868016a7a4ef175db69dea3ebba5f5113eec3
Gabbinetto <gabrieleferrante97@googlemail.com>
2024-10-10 23:48:07 +0200

    Added functionality for playlist files

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-Python class to download a YouTube playlist as .mp3 files. Requires [yt-dlp](https://github.com/yt-dlp/yt-dlp) executable to be in the same folder.
+Python class to download a YouTube playlist as .mp3 files. Requires [yt-dlp](https://github.com/yt-dlp/yt-dlp) executable to be in the same folder. If the executable isn't found, it will be downloaded from the official repository.
+
+## Using
+The program will create a folder with the same name of the playlist, but slugified. Then all the songs will be downloaded in that same folder as `<slugified-song-name>.mp3`. Finally, a `.m3u8` playlist file will be created as a sibling to the folder. This file must be a sibling to the playlist folder to work.
 
 Run `playlist-downloader.py`. If no link is passed as argument, it will be asked.
+
+Valid arguments:
+- **--no-meta**: won't add ID3 metadata to the `.mp3` files
+- **--no-m3u**: won't generate the `.m3u8` file
+- **--m3u-only**: will only generate the `.m3u8` file
+
+### Example
+If the user wants to download a playlist called "Cool playlist" that contains these songs:
+- Cool song 1
+- Cool song 2
+- Very cool song
+
+After running `playlist-downloader.py` with only the playlist link as an argument, the folder containing the program will look like this:
+```
+playlist-downloader.py
+yt-dlp.exe
+cool-playlist.m3u8
+cool-playlist/
+-- cool-song-1.mp3
+-- cool-song-2.mp3
+-- very-cool-song.mp3
+```
--- playlist-downloader.py
+++ playlist-downloader.py
@@ -7,39 +7,52 @@ import os
 import json
 import eyed3
 import urllib.request
+import getopt
 
 
 class Playlist:
-    def __init__(self, add_metadata: bool = True):
+    def __init__(self, add_metadata: bool = True, m3u: bool = True):
         self.videos: list[dict] = []  # type: ignore
         self.name: str
         self.length: int
         self.metadata: bool = add_metadata
+        self.make_m3u: bool = m3u
 
-    def __process_video(
-        self, video: dict, index: int, directory: str
-    ) -> None:
+        if not os.path.exists("yt-dlp.exe"):
+            print("yt-dlp.exe not found. Downloading executable from github.com...")
+            request = urllib.request.urlopen(
+                "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe"
+            )
+            with open("yt-dlp.exe", "wb") as f:
+                f.write(request.read())
+
+    def __process_video(self, video: dict, index: int, directory: str) -> str:
+        print(f"Processing {video['title']}...")
         file_path: str = os.path.join(directory, f"{slugify(video["title"])}.mp3")
         args: list[str] = [
-                ".\\yt-dlp.exe",
-                "--extract-audio",
-                "--audio-format", "mp3",
-                "--output", file_path,
-                video["url"]
-            ]
-        subprocess.run(
-            args
-        )
+            ".\\yt-dlp.exe",
+            "--extract-audio",
+            "--audio-format",
+            "mp3",
+            "--output",
+            file_path,
+            video["url"],
+        ]
+        subprocess.run(args, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         if not self.metadata:
-            return
+            print(f"{video['title']} downloaded.")
+            return file_path
 
         try:
+            print("Writing ID3 tags...")
             audiofile: eyed3.mp3.Mp3AudioFile = eyed3.load(file_path)
             audiofile.initTag(version=(2, 3, 0))
             audiofile.tag.title = video.get("title", "Unknown")
             audiofile.tag.artist = video.get("artist", video.get("uploader", "???"))
             audiofile.tag.album = video.get("album", video.get("channel", "???"))
-            audiofile.tag.album_artist = video.get("album_artist", video.get("uploader", "???"))
+            audiofile.tag.album_artist = video.get(
+                "album_artist", video.get("uploader", "???")
+            )
             audiofile.tag.composer = video.get("composer")
             audiofile.tag.genre = video.get("genre")
             audiofile.tag.play_count = video.get("view_count")
@@ -47,26 +60,33 @@ class Playlist:
             audiofile.tag.track_num = index + 1, self.length
 
             release_date: str = video.get("release_date", "20000101")
-            audiofile.tag.release_date = eyed3.core.Date(int(release_date[:4]), int(release_date[4:6]), int(release_date[6:8]))
+            audiofile.tag.release_date = eyed3.core.Date(
+                int(release_date[:4]), int(release_date[4:6]), int(release_date[6:8])
+            )
 
             if video["thumbnails"]:
                 thumb_response = urllib.request.urlopen(video["thumbnails"][-1]["url"])
                 thumbnail = thumb_response.read()
-                audiofile.tag.images.set(3, thumbnail, "image/webp", description="cover")
+                audiofile.tag.images.set(
+                    3, thumbnail, "image/webp", description="cover"
+                )
 
             audiofile.tag.save()
-            print("ID3 Tags set")
+            print("ID3 Tags written.")
         except IOError as e:
             print("Couldn't load mp3", video["title"], f"\n{e}")
 
+        print(f"{video['title']} downloaded.")
+        return file_path
 
     def fetch(self, url: str) -> Self:
+        print("Getting playlist data...")
         res: subprocess.CompletedProcess = subprocess.run(
             [
                 ".\\yt-dlp.exe",
                 "--flat-playlist",
                 "-j",
-                playlist_link,
+                url,
             ],
             stdout=subprocess.PIPE,
             stderr=subprocess.DEVNULL,
@@ -78,7 +98,10 @@ class Playlist:
 
         vid: str
         for vid in output:
-            self.videos.append(json.loads(vid))
+            video: dict = json.loads(vid)
+            if video["title"] == "[Deleted video]":
+                continue
+            self.videos.append(video)
 
         self.name = slugify(self.videos[0]["playlist_title"])
         self.length = len(self.videos)
@@ -97,16 +120,62 @@ class Playlist:
 
         return self
 
+    def save_m3u(self) -> Self:
+        m3u: self.M3U = self.M3U(self.name + ".m3u8")
+        for i, video in enumerate(self.videos):
+            file_path: str = self.name + "/" + f"{slugify(video["title"])}.mp3"
+            m3u.add_track(file_path, i + 1, video["title"])
+        m3u.save()
+
+        return self
+
+    class M3U:
+        def __init__(self, path: str = "") -> None:
+            self.path: str = path
+            self.content: list[dict] = []
+
+        def add_track(self, song_filename: str, track_num: int, name: str = "") -> None:
+            song: dict = {
+                "path": song_filename,
+                "track_num": track_num,
+                "name": name if name != "" else f"Track {track_num}",
+            }
+            self.content.append(song)
+            self.content.sort(key=lambda item: item["track_num"])
+
+        def save(self) -> None:
+            with open(self.path, "w", encoding="utf-8") as f:
+                f.write("#EXTM3U\n")
+                song: dict
+                for song in self.content:
+                    f.write(f"#EXTINF:-1, {song['name']}\n{song['path']}\n")
 
 
 if __name__ == "__main__":
-    playlist_link: str
-    if len(sys.argv) > 1:
-        playlist_link = sys.argv[1]
-    else:
-        playlist_link = input("Link to the playlist: ")
-    
-    if not playlist_link:
-        raise Exception("No link was passed")
+    args: list[str] = sys.argv[1:]
 
-    playlist: Playlist = Playlist(add_metadata = not ("--no-metadata" in sys.argv)).fetch(playlist_link).download()
+    link: str = ""
+    metadata: bool = True
+    m3u: bool = True
+    m3u_only: bool = False
+
+    if "--no-meta" in args:
+        args.remove("--no-meta")
+        metadata = False
+    if "--no-m3u" in args:
+        args.remove("--no-m3u")
+        m3u = False
+    if "--m3u-only" in args:
+        args.remove("--m3u-only")
+        m3u_only = True
+    if len(args) > 0:
+        link = args[0]
+    if link == "":
+        link = input("Input playlist link: ")
+
+    playlist = Playlist(metadata, m3u)
+    playlist.fetch(link)
+    if not m3u_only:
+        playlist.download()
+    if m3u:
+        playlist.save_m3u()
